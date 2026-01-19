60ab03a058836393eb3da2bf221aeb20f2d70199
Pingdum Official <66193372+pingdum@users.noreply.github.com>
2026-01-19 13:19:54 +0700

    Replace StorageUtils.deleteServerCookie with Cookies.remove

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import StorageUtils from "@/utils/storage";
+import Cookies from 'js-cookie';
 
 export default function LoginPage() {
   const handleLogin = () => {
     StorageUtils.setCookie("access_token", "dummy_token");
     const nextUrl = StorageUtils.getCookie("NEXT_URL") || "/";
-    StorageUtils.deleteServerCookie("NEXT_URL");
+    Cookies.remove('NEXT_URL');
     window.location.href = nextUrl;
   };
 
