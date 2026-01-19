3e8f72f9fbe5cc738168d0225b4c777592e776b9
Pingdum Official <66193372+pingdum@users.noreply.github.com>
2026-01-19 13:41:11 +0700

    Refactor deleteCookie and remove deleteServerCookie

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -16,20 +16,14 @@ const setCookie = (
   });
 };
 
-const deleteCookie = (key: string, options: Cookies.CookieAttributes = {}) => {
-  return Cookies.remove(key, {
-    domain: window.location.hostname,
-    expires: 0,
-    ...options,
-  });
-};
-
-const deleteServerCookie = (
+const deleteCookie = (
   key: string,
   options: Cookies.CookieAttributes = {},
+  domain?: string,
 ) => {
-  console.log("Deleting server cookie:", key);
+  // Set domain = "" if you are deleting a cookie set from the server
   return Cookies.remove(key, {
+    domain: domain || window.location.hostname,
     expires: 0,
     ...options,
   });
@@ -40,7 +34,6 @@ const StorageUtils = {
   setCookie,
   getCookie,
   deleteCookie,
-  deleteServerCookie,
 };
 
 export default StorageUtils;
