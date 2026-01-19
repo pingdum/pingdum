45d130303924bdde1bb3188c1f49afbd2ab8f58b
Pingdum Official <66193372+pingdum@users.noreply.github.com>
2026-01-19 13:02:21 +0700

    Add deleteServerCookie function to StorageUtils

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -24,11 +24,23 @@ const deleteCookie = (key: string, options: Cookies.CookieAttributes = {}) => {
   });
 };
 
+const deleteServerCookie = (
+  key: string,
+  options: Cookies.CookieAttributes = {},
+) => {
+  return Cookies.remove(key, {
+    domain: "https://pingdum.vercel.app",
+    expires: 0,
+    ...options,
+  });
+};
+
 // export module
 const StorageUtils = {
   setCookie,
   getCookie,
   deleteCookie,
+  deleteServerCookie,
 };
 
 export default StorageUtils;
